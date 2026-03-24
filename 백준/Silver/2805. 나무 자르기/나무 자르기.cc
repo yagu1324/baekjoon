8515cdb5f8f0921d4f609d5cd30a5ab4524c7111
yagu1324 <joosin1324@gmail.com>
2026-03-24 18:01:26 +0900

    [Silver II] Title: 나무 자르기, Time: 168 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -11,12 +11,15 @@ using namespace std;
 // 
 // 이건 위의처럼 무식한 방법이 아닌 이분 탐색을 해야합니다
 // 2번째 방법 -> 원래는 하나의 상한선을 통하여 값들을 줄이고 올리고 할 생각이었습.
-// 문제발생) 근데 이렇게 하니까 무한루프가 발생함 11/10 -> 0 이런느낌으로
+// 문제발생) 근데 이렇게 하니까 뭔가 저장해야될 값들이 많아 짐 past 하나로는 부족하다고 느낌
+// 막상 해보니까 상한선이 만약 위에서 아래로 내려왔을 경우 올라가는건 막겠지만 내려가는건 결국 shortest가 제일밑임
+// 만약 밑에서 위로 cut가 올라간다고 가정했을 경우, 밑에서 올라오는 past가 없다면 결국 cut는 또 내려감
 // 
 // 그렇다면? 상한선을 1개가 아닌 2개로 바꿈 -> 맨위와 맨 아래
 // -> 100 0 이렇게 있으면 50 0 -> 50 25 이런식으로
-// 문제 발생 ) 11/10 -> 이렇게 되면 0이 나옴 -> 해결) 강제로 -1, +1 을 실행
+// 문제 발생 ) 11/10 -> 이렇게 되면 0이 나옴(무한루프에 빠짐) -> 해결) 강제로 -1, +1 을 실행
 // 높은곳에서는 -1. 낮은곳에서는 +1/ 대신 while 문을 true 에서 shortest<=longest로 변경
+//									-> 이렇게 한 이유는 longest가 need 값을 찍었을 경우 49+49 / 2 = cut 이라는 공식으로 결국 값을 저장
 // (이부분은 ai에게 도움받음)
 //
 
@@ -27,9 +30,10 @@ int main() {
 
 	int tree_case; // 입력받을 나무의 갯수
 	int need;// 필요한 나무 길이
-	int a,longest=0,shortest=0,cut=0;// a = 대충 입력받을 값, longest = 가장 긴 나무길이, sum=자를 나무길이의 총 합
-	long long sum = 0,ans;
+	int a,longest=0,shortest=0,cut=0;// a = 대충 입력받을 값, longest = 가장 긴 나무길이
+	long long sum = 0,ans; //sum = 자른 나무의 총 길이, ans = 답을 저장할 값
 	vector<int> tree;
+
 	//입력들
 	cin >> tree_case;
 	cin >> need;
@@ -41,7 +45,7 @@ int main() {
 	cut = longest;
 
 	
-	while (shortest<=longest) {
+	while (shortest<=longest) { 
 
 		sum = 0;
 		for (int i = 0; i < tree_case; i++) { // sum 값
@@ -63,53 +67,3 @@ int main() {
 	return 0;
 }
 
-//// ai가 푼거임 뭐가다른거냐 시발
-//#include <iostream>
-//#include <vector>
-//
-//using namespace std;
-//
-//int main() {
-//    // 빠른 입출력 (필수)
-//    ios_base::sync_with_stdio(false);
-//    cin.tie(NULL);
-//
-//    int tree_case;
-//    long long need;
-//    cin >> tree_case >> need;
-//
-//    // 💡 핵심 최적화: 미리 필요한 만큼 벡터의 크기를 할당해 둠 (push_back 제거)
-//    vector<int> tree(tree_case);
-//    long long longest = 0;
-//
-//    for (int i = 0; i < tree_case; i++) {
-//        cin >> tree[i]; // push_back 대신 인덱스로 바로 접근
-//        if (longest < tree[i]) longest = tree[i];
-//    }
-//
-//    long long shortest = 0;
-//    long long ans = 0;
-//
-//    // 이분 탐색 뼈대 (무한루프 원천 차단)
-//    while (shortest <= longest) {
-//        long long cut = (shortest + longest) / 2;
-//        long long sum = 0;
-//
-//        for (int i = 0; i < tree_case; i++) {
-//            if (tree[i] > cut) {
-//                sum += tree[i] - cut;
-//            }
-//        }
-//
-//        if (sum < need) {
-//            longest = cut - 1;
-//        }
-//        else {
-//            ans = cut;
-//            shortest = cut + 1;
-//        }
-//    }
-//
-//    cout << ans << "\n";
-//    return 0;
-//}
