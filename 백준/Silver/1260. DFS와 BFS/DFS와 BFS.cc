7117b9c238fa23329f9a2b23f7be59926986ca72
yagu1324 <joosin1324@gmail.com>
2026-03-24 17:15:23 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+const int MAX = 1001; // 정점의 개수 N은 최대 1,000
+vector<int> adj[MAX];
+bool visited_dfs[MAX];
+bool visited_bfs[MAX];
+
+// DFS 함수 (재귀 방식)
+void dfs(int node) {
+    visited_dfs[node] = true; // 간곳이라는 표시
+    cout << node << " "; //출력
+
+    for (int i = 0; i < adj[node].size(); i++) {
+        int next = adj[node][i];
+        if (!visited_dfs[next]) { // false 일 경우 못감
+            dfs(next);
+        }
+    }
+}
+
+// BFS 함수 (큐 방식)
+void bfs(int start) {
+    queue<int> q;
+    q.push(start);
+    visited_bfs[start] = true;// 간곳이라는 표시
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        cout << node << " ";
+
+        for (int i = 0; i < adj[node].size(); i++) {
+            int next = adj[node][i];
+            if (!visited_bfs[next]) {
+                visited_bfs[next] = true;
+                q.push(next);
+            }
+        }
+    }
+}
+
+int main() {
+    // 입출력 속도 향상
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n, m, v;
+    cin >> n >> m >> v;
+
+    // 간선 정보 입력받기
+    for (int i = 0; i < m; i++) {
+        int u, w;
+        cin >> u >> w; 
+        // 양방향 간선이므로 양쪽에 추가
+        adj[u].push_back(w);
+        adj[w].push_back(u); 
+    }
+
+    // 내림차순을 통해서 정렬하기
+    for (int i = 1; i <= n; i++) {
+        sort(adj[i].begin(), adj[i].end());
+    }
+
+    // DFS 실행 및 결과 출력
+    dfs(v);
+    cout << "\n";
+
+    // BFS 실행 및 결과 출력
+    bfs(v);
+    cout << "\n";
+
+    return 0;
+}
